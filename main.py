1da5e37b457c9a11b171bfddc7b7c29e7055a33b
Vinicius Silva <viniciussds2000@Hotmail.com>
2019-04-12 15:13:25 -0300

    Base de templates

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/adm_page')
+def adm_page():
+    return render_template('adm_page.html')
+
+@app.route('/detalhes')
+def detalhes_compra():
+    return render_template('detalhes.html')
+
+@app.route('/final_reserva')
+def fin_compra():
+    return render_template('tela_compra.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
